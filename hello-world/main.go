9f053d4389eb5720a4c77b879b0106c9c2826d73
santictz <cortezsantiago@outlook.com>
2021-09-28 23:44:00 -0300

    Types

--- hello-world/main.go
+++ hello-world/main.go
@@ -9,7 +9,14 @@ func main() {
 	//Basic output
 	fmt.Println("Hello World")
 
-	//Initialize variables
+	//Variables in Go
+	variables()
+
+	//Types in Go
+	types()
+}
+
+func variables() {
 	var age int = 28
 	fmt.Println("My age is", age)
 
@@ -34,4 +41,33 @@ func main() {
 	a, b := 10.4, 20.5
 	c := math.Min(a, b)
 	fmt.Println("The minimum value is:", c)
+}
+
+func types() {
+	//Booleans
+	a := true
+	b := false
+	fmt.Println("a:", a, "b:", b)
+
+	c := a && b
+	fmt.Println("c:", c)
+
+	d := a || b
+	fmt.Println("d:", d)
+
+	//%T shows the type
+	f1, f2 := 2.45, 23.3
+	fmt.Println("Type of f1 %T f2 %T\n", f1, f2)
+
+	//Strings
+	first := "Santiago"
+	last := "Cortez"
+	name := first + " " + last
+	fmt.Println("My name is", name)
+
+	//Type conversion
+	i := 22
+	j := 92.2
+	sum := i + int(j)
+	fmt.Println("Sum is:", sum)
 }
